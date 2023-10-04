6f3f1139ddc272d817fe815482366a17ea13c5cf
Jay <me@bsavage.xyz>
2023-10-04 19:00:18 +0530

    replaced stargate logging with Apache extended format

--- middleware/logging.go
+++ middleware/logging.go
@@ -3,11 +3,13 @@ package middleware
 import (
 	"bufio"
 	"errors"
+	"fmt"
 	"io"
-	"log"
 	"net"
 	"net/http"
+	"net/url"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/realbucksavage/stargate"
@@ -36,19 +38,75 @@ func Logging() stargate.MiddlewareFunc {
 	return LoggingWithOutput(os.Stdout)
 }
 
-func LoggingWithOutput(dst io.Writer) stargate.MiddlewareFunc {
+func LoggingWithOutput(out io.Writer) stargate.MiddlewareFunc {
+	return writeAccessLogs(out)
+}
 
-	logger := log.New(dst, "[stargate.requests] ", log.LstdFlags)
+func writeAccessLogs(out io.Writer) stargate.MiddlewareFunc {
 	return func(next http.Handler) http.Handler {
-
 		return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-
 			lrw := &loggingResponseWriter{ResponseWriter: rw, status: http.StatusOK}
-			defer func(begin time.Time) {
-				logger.Printf("[%s | %d] %s\t(%v)", r.Method, lrw.status, r.RequestURI, time.Since(begin))
-			}(time.Now())
+			defer func() {
+				out.Write(buildCommonLogLine(r, *r.URL, time.Now(), lrw.status, 0))
+			}()
 
 			next.ServeHTTP(lrw, r)
 		})
 	}
+}
+
+// buildCommonLogLine builds a log entry for req in Apache Common Log Format.
+// ts is the timestamp with which the entry should be logged.
+// status and size are used to provide the response HTTP status and size.
+func buildCommonLogLine(req *http.Request, url url.URL, ts time.Time, status int, size int) []byte {
+	username := "-"
+	if url.User != nil {
+		if name := url.User.Username(); name != "" {
+			username = name
+		}
+	}
+
+	host, _, err := net.SplitHostPort(req.RemoteAddr)
+	if err != nil {
+		host = req.RemoteAddr
+	}
+
+	xff := req.Header.Get("X-Forwarded-For")
+	if xff != "" {
+		host = xff
+	}
+
+	uri := req.RequestURI
+
+	// Requests using the CONNECT method over HTTP/2.0 must use
+	// the authority field (aka r.Host) to identify the target.
+	// Refer: https://httpwg.github.io/specs/rfc7540.html#CONNECT
+	if req.ProtoMajor == 2 && req.Method == "CONNECT" {
+		uri = req.Host
+	}
+	if uri == "" {
+		uri = url.RequestURI()
+	}
+
+	buf := make([]byte, 0, 3*(len(host)+len(username)+len(req.Method)+len(uri)+len(req.Proto)+50)/2)
+	buf = append(buf, host...)
+	buf = append(buf, " - "...)
+	buf = append(buf, username...)
+	buf = append(buf, " ["...)
+	buf = append(buf, ts.Format("02/Jan/2006:15:04:05 -0700")...)
+	buf = append(buf, `] "`...)
+	buf = append(buf, req.Method...)
+	buf = append(buf, " "...)
+	buf = append(buf, fmt.Sprintf("%q", uri)...)
+	buf = append(buf, " "...)
+	buf = append(buf, req.Proto...)
+	buf = append(buf, `" `...)
+	buf = append(buf, strconv.Itoa(status)...)
+	buf = append(buf, " "...)
+	buf = append(buf, strconv.Itoa(size)...)
+	buf = append(buf, ` `...)
+	buf = append(buf, fmt.Sprintf("%q", req.Referer())...)
+	buf = append(buf, ` `...)
+	buf = append(buf, fmt.Sprintf("%q", req.UserAgent())...)
+	return buf
 }
